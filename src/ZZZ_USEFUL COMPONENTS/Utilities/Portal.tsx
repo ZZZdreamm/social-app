b5786a786bc477e01f00fa7113d8dd2d02bcb7c2
ZZZdream <kmultan1234@gmail.com>
2023-07-15 19:01:14 +0200

    Added choosenPortal component

--- src/ZZZ_USEFUL COMPONENTS/Utilities/Portal.tsx
+++ src/ZZZ_USEFUL COMPONENTS/Utilities/Portal.tsx
@@ -6,3 +6,11 @@ export default function Portal({ children }: any) {
     }
     return ReactDOM.createPortal(children, document.body);
   }
+
+
+export function ChoosenPortal({ children, portalDirection }: any) {
+    if (typeof document == "undefined") {
+      return <>{children}</>;
+    }
+    return ReactDOM.createPortal(children, portalDirection);
+  }
