c4da2b2e75e46b82f350927dffc538a2fa029d2d
ZZZdream <kmultan1234@gmail.com>
2023-07-15 22:11:54 +0200

    Message component splitted into smaller parts

--- src/components/Messages/Message/index.tsx
+++ src/components/Messages/Message/index.tsx
@@ -1,5 +1,8 @@
 import { useContext, useEffect, useState } from "react";
-import { messageDTO, messageResponseDTO } from "../../../services/Models/message.models";
+import {
+  messageDTO,
+  messageResponseDTO,
+} from "../../../services/Models/message.models";
 import ProfileContext from "../../../services/Contexts/ProfileContext";
 import BigImageModal from "../../../ZZZ_USEFUL COMPONENTS/Utilities/BigImageModal";
 import { ReadyImagesURL } from "../../../globals/appUrls";
@@ -33,7 +36,7 @@ export default function Message({
   const [isOpen, setIsOpen] = useState(false);
   const [optionsVisible, setOptionsVisible] = useState(false);
 
-  function toggleModal(isOpen: any, setIsOpen: any) {
+  function toggleModal() {
     setIsOpen(!isOpen);
   }
 
@@ -46,21 +49,6 @@ export default function Message({
       setFromFriend(true);
     }
   }, [myProfile]);
-  const contentStyle = fromFriend
-    ? { backgroundColor: "#E4E6EB" }
-    : { backgroundColor: "#0084ff" };
-  const color = fromFriend ? "#2f2f2f" : "#ffffff";
-
-  const gridSize =
-    message.MediaFiles.length < 4 ? message.MediaFiles.length : 4;
-
-  const responseGridSize =
-    message.responseTo && message.responseTo.MediaFiles.length < 4
-      ? message.MediaFiles.length
-      : 4;
-  function showBigImages() {
-    // setIsOpen(true);
-  }
   return (
     <div
       className="message"
@@ -72,82 +60,12 @@ export default function Message({
         setOptionsVisible(false);
       }}
     >
-      <BigImageModal
+      <BigImageShown
+        message={message}
         isOpen={isOpen}
         toggleModal={toggleModal}
-        children={
-          <div
-            className="full-container flex-column-center"
-            style={{ height: "100vh" }}
-          >
-            <span className="header">
-              <img
-                className="pointer"
-                src={`${ReadyImagesURL}/goBackArrow.png`}
-                onClick={() => toggleModal(isOpen, setIsOpen)}
-                alt=""
-              />
-            </span>
-            <span className="body" style={{ height: "90%" }}>
-              <ScrollingMediaFiles mediaFiles={message.MediaFiles} />
-            </span>
-          </div>
-        }
       />
-
-      <div
-        className="message-content medium-font"
-        style={{ color: color, ...contentStyle }}
-      >
-        {message.responseTo && (
-          <div className="message-responseTo">
-            <>
-              <div className="message-responseTo-content">
-                <div className="message-responseTo-content__autor">
-                  Respond to{" "}
-                  <span className="bolder">
-                    {message.responseTo.SenderName}
-                  </span>
-                </div>
-                <span className="message-responseTo-content__text">
-                  {message.responseTo.TextContent}
-                </span>
-                {message.responseTo.MediaFiles.length > 0 && (
-                  <div
-                    className="message-responseTo-content__media"
-                    style={{
-                      gridTemplateColumns: `repeat(${responseGridSize}, 1fr)`,
-                    }}
-                  >
-                    {message.responseTo.MediaFiles.map((mediaFile) => (
-                      <img key={mediaFile} src={mediaFile} alt="" />
-                    ))}
-                  </div>
-                )}
-              </div>
-            </>
-          </div>
-        )}
-        {message.TextContent}
-        {message.MediaFiles.length > 0 && (
-          <div
-            className="message-content-media"
-            onClick={showBigImages}
-            style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
-          >
-            {message.MediaFiles.map((mediaFile) => (
-              <img key={mediaFile} src={mediaFile} alt="" />
-            ))}
-          </div>
-        )}
-        {message.VoiceFile && (
-          <audio
-            className="message-content-audio"
-            src={message.VoiceFile}
-            controls={true}
-          />
-        )}
-      </div>
+      <MessageContent message={message} fromFriend={fromFriend} />
       {notResponding && (
         <div style={{ opacity: optionsVisible ? "1" : "0" }}>
           <MessageOptions
@@ -163,3 +81,113 @@ export default function Message({
     </div>
   );
 }
+
+interface MessageChildProps {
+  message: messageDTO;
+}
+
+interface MessageContentProps extends MessageChildProps {
+  fromFriend: boolean;
+}
+
+const MessageContent = ({ message, fromFriend }: MessageContentProps) => {
+  const contentStyle = fromFriend
+    ? { backgroundColor: "#E4E6EB" }
+    : { backgroundColor: "#0084ff" };
+  const color = fromFriend ? "#2f2f2f" : "#ffffff";
+
+  const gridSize =
+    message.MediaFiles.length < 4 ? message.MediaFiles.length : 4;
+
+  const responseGridSize =
+    message.responseTo && message.responseTo.MediaFiles.length < 4
+      ? message.MediaFiles.length
+      : 4;
+  return (
+    <div
+      className="message-content medium-font"
+      style={{ color: color, ...contentStyle }}
+    >
+      {message.responseTo && (
+        <div className="message-responseTo">
+          <>
+            <div className="message-responseTo-content">
+              <div className="message-responseTo-content__autor">
+                Respond to{" "}
+                <span className="bolder">{message.responseTo.SenderName}</span>
+              </div>
+              <span className="message-responseTo-content__text">
+                {message.responseTo.TextContent}
+              </span>
+              {message.responseTo.MediaFiles.length > 0 && (
+                <div
+                  className="message-responseTo-content__media"
+                  style={{
+                    gridTemplateColumns: `repeat(${responseGridSize}, 1fr)`,
+                  }}
+                >
+                  {message.responseTo.MediaFiles.map((mediaFile) => (
+                    <img key={mediaFile} src={mediaFile} alt="" />
+                  ))}
+                </div>
+              )}
+            </div>
+          </>
+        </div>
+      )}
+      {message.TextContent}
+      {message.MediaFiles.length > 0 && (
+        <div
+          className="message-content-media"
+          style={{ gridTemplateColumns: `repeat(${gridSize}, 1fr)` }}
+        >
+          {message.MediaFiles.map((mediaFile) => (
+            <img key={mediaFile} src={mediaFile} alt="" />
+          ))}
+        </div>
+      )}
+      {message.VoiceFile && (
+        <audio
+          className="message-content-audio"
+          src={message.VoiceFile}
+          controls={true}
+        />
+      )}
+    </div>
+  );
+};
+
+interface BigImageModalProps extends MessageChildProps {
+  isOpen: boolean;
+  toggleModal: any;
+}
+const BigImageShown = ({
+  message,
+  isOpen,
+  toggleModal,
+}: BigImageModalProps) => {
+  return (
+    <BigImageModal
+      isOpen={isOpen}
+      toggleModal={toggleModal}
+      children={
+        <div
+          className="full-container flex-column-center"
+          style={{ height: "100vh" }}
+        >
+          <span className="header">
+            <img
+              className="pointer"
+              src={`${ReadyImagesURL}/goBackArrow.png`}
+              onClick={toggleModal}
+              alt=""
+            />
+          </span>
+          <span className="body" style={{ height: "90%" }}>
+            <ScrollingMediaFiles mediaFiles={message.MediaFiles} />
+          </span>
+        </div>
+      }
+    />
+  );
+};
