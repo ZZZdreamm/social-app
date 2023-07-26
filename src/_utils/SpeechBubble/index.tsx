c05df88005785d47a9a65d903287a74973974c25
ZZZdream <kmultan1234@gmail.com>
2023-07-26 15:26:25 +0200

    SpeechBubble component implemented

---
+++ src/_utils/SpeechBubble/index.tsx
@@ -0,0 +1,11 @@
+import "./style.scss";
+
+export default function SpeechBubble() {
+  return (
+    //@ts-ignore
+    <div speech-bubble ptop acenter>
+      <div className="title">Top Center</div>
+    </div>
+  );
+}
+
