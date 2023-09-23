34f16c6ad497480e5fc4946907bf00e83304bd1b
ZZZdream <kmultan1234@gmail.com>
2023-09-23 18:01:12 +0200

    Add headers to axios requests

--- src/globals/apiPaths.ts
+++ src/globals/apiPaths.ts
@@ -10,23 +10,34 @@ if (process.env.NODE_ENV == `development`) {
   socketURL = localSocketURL;
 }
 
+const headers = {
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "Content-Type",
+};
+
 export const axiosBase = axios.create({
   baseURL: serverURL,
+  headers: headers,
 });
 
 export const axiosBasePosts = axios.create({
   baseURL: serverURL + "posts/",
+  headers: headers,
 });
 
 export const axiosBaseProfiles = axios.create({
-    baseURL: serverURL + "profiles/",
+  baseURL: serverURL + "profiles/",
+  headers: headers,
 });
 
 export const axiosBaseComments = axios.create({
-    baseURL: serverURL + "comments/",
+  baseURL: serverURL + "comments/",
+  headers: headers,
 });
 
 export const axiosBaseMessages = axios.create({
-    baseURL: serverURL + "messages/",
+  baseURL: serverURL + "messages/",
+  headers: headers,
 });
-
