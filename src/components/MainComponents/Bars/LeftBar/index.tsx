6c7dc7168f8c12c41383bace905b7c9fba7f2ca2
ZZZdream <kmultan1234@gmail.com>
2023-10-11 21:28:34 +0200

    Add condition for not rendering LeftBar if in /reels

--- src/components/MainComponents/Bars/LeftBar/index.tsx
+++ src/components/MainComponents/Bars/LeftBar/index.tsx
@@ -7,6 +7,7 @@ import { ProfileImage } from "../../../ProfileImage/ProfileImage";
 import { useOpenedChatsContext } from "../../../../services/Contexts/OpenedChatsContext";
 import { useAuthenticationContext } from "../../../../services/Contexts/AuthenticationContext";
 
+
 export default function LeftBar() {
   const navigate = useNavigate();
   const { profile } = useAuthenticationContext();
@@ -50,90 +51,94 @@ export default function LeftBar() {
     };
   }
   return (
-    <nav className="bar bar-left" style={barStyling}>
-      {profile && (
-        <ul>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              navigate(`/`);
-            }}
-          >
-            {location.pathname == "/" && (
-              <div className="bar-left__active"></div>
-            )}
-            <img src={`${ReadyImagesURL}/homepage.png`} alt="" />
-            {fullBar && <span className="medium-font">Home</span>}
-          </li>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              navigate(`user-profile/${profile.Id}`);
-            }}
-          >
-            {location.pathname.includes("user-profile") && (
-              <div className="bar-left__active"></div>
-            )}
-            <ProfileImage imageURL={profile.ProfileImage} />
-            {fullBar && <span className="medium-font">{profile.Email}</span>}
-          </li>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              navigate(`/user-friends/${profile.Id}`);
-            }}
-          >
-            {location.pathname.includes("user-friends") && (
-              <div className="bar-left__active"></div>
-            )}
+    <>
+    {!location.pathname.includes('reels') &&
+      <nav className="bar bar-left" style={barStyling}>
+        {profile && (
+          <ul>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                navigate(`/`);
+              }}
+            >
+              {location.pathname == "/" && (
+                <div className="bar-left__active"></div>
+              )}
+              <img src={`${ReadyImagesURL}/homepage.png`} alt="" />
+              {fullBar && <span className="medium-font">Home</span>}
+            </li>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                navigate(`user-profile/${profile.Id}`);
+              }}
+            >
+              {location.pathname.includes("user-profile") && (
+                <div className="bar-left__active"></div>
+              )}
+              <ProfileImage imageURL={profile.ProfileImage} />
+              {fullBar && <span className="medium-font">{profile.Email}</span>}
+            </li>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                navigate(`/user-friends/${profile.Id}`);
+              }}
+            >
+              {location.pathname.includes("user-friends") && (
+                <div className="bar-left__active"></div>
+              )}
 
-            <img src={`${ReadyImagesURL}/friends.png`} alt="" />
-            {fullBar && <span className="medium-font">Friends</span>}
-          </li>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              navigate(`/user-friend-requests/${profile.Id}`);
-            }}
-          >
-            {location.pathname.includes("user-friend-requests") && (
-              <div className="bar-left__active"></div>
-            )}
+              <img src={`${ReadyImagesURL}/friends.png`} alt="" />
+              {fullBar && <span className="medium-font">Friends</span>}
+            </li>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                navigate(`/user-friend-requests/${profile.Id}`);
+              }}
+            >
+              {location.pathname.includes("user-friend-requests") && (
+                <div className="bar-left__active"></div>
+              )}
 
-            <img src={`${ReadyImagesURL}/goBackArrow.png`} alt="" />
-            {fullBar && <span className="medium-font">Friend Requests</span>}
-          </li>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              navigate(`/user-sent-friend-requests/${profile.Id}`);
-            }}
-          >
-            {location.pathname.includes("user-sent-friend-requests") && (
-              <div className="bar-left__active"></div>
-            )}
+              <img src={`${ReadyImagesURL}/goBackArrow.png`} alt="" />
+              {fullBar && <span className="medium-font">Friend Requests</span>}
+            </li>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                navigate(`/user-sent-friend-requests/${profile.Id}`);
+              }}
+            >
+              {location.pathname.includes("user-sent-friend-requests") && (
+                <div className="bar-left__active"></div>
+              )}
 
-            <img
-              style={{ rotate: "180deg" }}
-              src={`${ReadyImagesURL}/goBackArrow.png`}
-              alt=""
-            />
-            {fullBar && (
-              <span className="medium-font">Sent Friend Requests</span>
-            )}
-          </li>
-          <li
-            style={elementsStyling}
-            onClick={() => {
-              setOpenedChats([]);
-              navigate(`/messaging-only/${profile.Id}`);
-            }}
-          >
-            <img src={`${ReadyImagesURL}/messaging-only.png`} alt="" />
-            {fullBar && <span className="medium-font">Messaging only</span>}
-          </li>
-        </ul>
-      )}
-    </nav>
+              <img
+                style={{ rotate: "180deg" }}
+                src={`${ReadyImagesURL}/goBackArrow.png`}
+                alt=""
+              />
+              {fullBar && (
+                <span className="medium-font">Sent Friend Requests</span>
+              )}
+            </li>
+            <li
+              style={elementsStyling}
+              onClick={() => {
+                setOpenedChats([]);
+                navigate(`/messaging-only/${profile.Id}`);
+              }}
+            >
+              <img src={`${ReadyImagesURL}/messaging-only.png`} alt="" />
+              {fullBar && <span className="medium-font">Messaging only</span>}
+            </li>
+          </ul>
+        )}
+      </nav>
+}
+    </>
   );
 }
