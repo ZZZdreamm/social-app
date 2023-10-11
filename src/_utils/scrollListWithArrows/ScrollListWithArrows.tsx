b4f1f9d238a9a53d87d8d9c4c2c30e558f16e5c3
ZZZdream <kmultan1234@gmail.com>
2023-10-11 21:22:32 +0200

    Add Scrolling Arrows for lists

---
+++ src/_utils/scrollListWithArrows/ScrollListWithArrows.tsx
@@ -0,0 +1,73 @@
+import { useEffect, useRef, useState } from "react";
+import { ReadyImagesURL } from "../../globals/appUrls";
+import "./style.scss";
+
+interface ScrollingMediaFilesProps {
+  containerRef: React.MutableRefObject<HTMLDivElement | null>;
+}
+
+export function ScrollListWithArrows({
+  containerRef,
+}: ScrollingMediaFilesProps) {
+  const leftScrollRef = useRef<HTMLDivElement | null>(null);
+  const rightScrollRef = useRef<HTMLDivElement | null>(null);
+
+  const [isScrolledToEnd, setIsScrolledToEnd] = useState(false);
+  const [isScrollAtStart, setIsScrollAtStart] = useState(false);
+
+  useEffect(() => {
+    setIsScrollAtStart(true);
+  }, []);
+
+  useEffect(() => {
+    if (
+      !containerRef.current ||
+      !leftScrollRef.current ||
+      !rightScrollRef.current
+    )
+      return;
+    leftScrollRef.current.addEventListener("click", () => {
+      containerRef.current!.scrollBy({
+        left: -containerRef.current!.offsetWidth,
+        behavior: "smooth",
+      });
+    });
+    rightScrollRef.current.addEventListener("click", () => {
+      containerRef.current!.scrollBy({
+        left: containerRef.current!.offsetWidth,
+        behavior: "smooth",
+      });
+    });
+
+    const handleScroll = () => {
+      const { scrollLeft, scrollWidth, clientWidth } = containerRef.current!;
+
+      const isAtEnd = Math.floor(scrollLeft) + clientWidth >= scrollWidth - 10;
+      const isAtStart = scrollLeft == 0;
+      setIsScrolledToEnd(isAtEnd);
+      setIsScrollAtStart(isAtStart);
+    };
+
+    containerRef.current.addEventListener("scroll", handleScroll);
+  }, [
+    containerRef,
+    leftScrollRef,
+    rightScrollRef,
+    isScrolledToEnd,
+    isScrollAtStart,
+  ]);
+  return (
+    <>
+      {!isScrollAtStart && (
+        <div ref={leftScrollRef} className="scroll scroll-left">
+          <img src={`${ReadyImagesURL}/goBackArrow.png`} alt="" />
+        </div>
+      )}
+      {!isScrolledToEnd && (
+        <div ref={rightScrollRef} className="scroll scroll-right">
+          <img src={`${ReadyImagesURL}/goBackArrow.png`} alt="" />
+        </div>
+      )}
+    </>
+  );
+}
